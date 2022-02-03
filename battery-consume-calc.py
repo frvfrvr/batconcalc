272797ebbc1342f5db4ed423cdb71eb5b481bbd1
frvfrvr <68894029+frvfrvr@users.noreply.github.com>
2022-02-04 00:36:48 +0800

    added log, nxt day interval should be fixed

--- battery-consume-calc.py
+++ battery-consume-calc.py
@@ -1,25 +1,35 @@
+
 # ! Battery Consumption Calculator
 # ! Author: @frvfrvr (github.com/frvfrvr)
-# ! v1 Development Time and Date: Jan-30-2022 6:23 PM - Jan-31-2022 6:34 PM
+# ! v0.1 Development Time and Date: Jan-30-2022 6:23 PM - Jan-31-2022 6:34 PM
 
 from tkinter import *
 import psutil
-import datetime
+from datetime import *
+from tkinter.filedialog import asksaveasfile
+import os, subprocess, platform
+
 
 # ? initiliaze the tkinter window
+
+currtoday = datetime.now().strftime("bcc-%I%M%p-%B-%d-%Y")
+filestore = []
 
 batthr = []
 battmin = []
 battsec = []
 battpercent = []
 
-def comma_insert(a, b):
-    # if another element exists, insert comma
-    if bool(a) and bool(b) == True:
-        return ", "
-    return ""
+def convert(seconds):
+    min, sec = divmod(seconds, 60)
+    hour, min = divmod(min, 60)
+    x = lambda x: x > 1 and "s" or "" # lambda function to check if x > 1 and return "s" or "" - makes word plural
+    comma = lambda comma: bool(comma) and ", " or "" # lambda function to check if comma > 0 and return "," or "". add comma if > 0. adds comma if there's another number after it
+    minsec = True if (bool(min) == bool(sec) == True) else False if (bool(min) == bool(sec) == False) else False # if min and sec are both true or both false, return False
+    hourmin = True if (bool(hour) == bool(min) == True) else False if (bool(hour) == bool(min) == False) else False # checks if hour > 0 or min > 0
+    return f"{(lambda hr: hr > 0 and f'{hr} ' or '')(hour)}{(lambda hr: hr > 0 and 'hour' or '')(hour)}{x(hour)}{comma(hourmin)}{(lambda m: m > 0 and f'{m} ' or '')(min)}{(lambda m: m > 0 and 'minute' or '')(min)}{x(min)}{comma(minsec)}{(lambda s: s > 0 and f'{s} ' or '')(sec)}{(lambda s: s > 0 and 'second' or '')(sec)}{x(sec)}"
 
-def batteryConsumption(dt, batthr, battmin, battsec, battpercent, simple=False):
+def batteryConsumption(dt, batthr, battmin, battsec, battpercent, simple=False, multilog=False):
     # requires datetime, battery hours, battery minutes, battery seconds, battery percentage    
     battery = psutil.sensors_battery()
     dthr = int(dt.strftime("%H"))
@@ -45,26 +55,25 @@ def batteryConsumption(dt, batthr, battmin, battsec, battpercent, simple=False):
                     battsec.append(dtsec) 
                     batthr.append(dthr) 
                     battmin.append(dtmin) 
-                # ? MATH PART
-                # ? battery life in seconds
-                b_sec = battsec[-1] - battsec[-2] if len(battsec) != 0 else 0
-                # ? battery life in minutes
-                b_min = battmin[-1] - battmin[-2] if len(battmin) != 0 else 0
-                # ? battery life in hours
-                b_hr = batthr[-1] - batthr[-2] if len(batthr) != 0 else 0
-                # ? STRING PART
-                # abs() absolute is used to prevent negative values
-                ba_hr = str(abs(b_hr)) + " hours" if abs(b_hr) > 1 else str(abs(b_hr)) + " hour" if b_hr == 1 else ""
-                ba_min = str(abs(b_min)) + " minutes" if abs(b_min) > 1 else str(abs(b_min)) + " minute" if b_min == 1 else ""
-                ba_sec = str(abs(b_sec)) + " seconds" if abs(b_sec) > 1 else str(abs(b_sec)) + " second" if b_sec == 1 else ""
-                if len(battpercent) > 2:
-                    del battpercent[:-2]
-                    del batthr[:-2]
-                    del battmin[:-2]
-                    del battsec[:-2]
-                #print(bool(ba_hr), bool(ba_min), bool(ba_sec))
-                #print(comma_insert(ba_hr, ba_min))
-                return "Battery consumes 1% every " + ba_hr + comma_insert(ba_hr, ba_min) + ba_min + comma_insert(ba_min, ba_sec) + ba_sec
+                twodgts = lambda x: x > 9 and f'{x}' or f'0{x}' # so the number is formmated as a 2 digit number
+                s1 = str(twodgts(batthr[-2])) + ':' + str(twodgts(battmin[-2])) + ':' + str(twodgts(battsec[-2])) # before previous time
+                s2 = str(twodgts(batthr[-1])) + ':' + str(twodgts(battmin[-1])) + ':' + str(twodgts(battsec[-1])) # current time
+                FMT = '%H:%M:%S'
+                tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
+                if tdelta.days < 0:
+                    tdelta = timedelta(
+                        days=0,
+                        seconds=tdelta.seconds,
+                        microseconds=tdelta.microseconds
+                    )
+                hourminsec = int(tdelta.total_seconds())
+                if multilog == False:
+                    if len(battpercent) > 2:
+                        del battpercent[:-2]
+                        del batthr[:-2]
+                        del battmin[:-2]
+                        del battsec[:-2]
+                return "Battery consumes 1% every " + convert(hourminsec)
             return "Data invalid. Anyway here's the current battery percentage: " + str(battery.percent) + "%"
         else:
             #return "Battery is plugged in, can't calculate consumption"
@@ -82,66 +91,101 @@ def batteryConsumption(dt, batthr, battmin, battsec, battpercent, simple=False):
                     battsec.append(dtsec) 
                     batthr.append(dthr) 
                     battmin.append(dtmin) 
-                # ? MATH PART
-                # ? battery life in seconds
-                b_sec = battsec[-1] - battsec[-2] if len(battsec) != 0 else 0
-                # ? battery life in minutes
-                b_min = battmin[-1] - battmin[-2] if len(battmin) != 0 else 0
-                # ? battery life in hours
-                b_hr = batthr[-1] - batthr[-2] if len(batthr) != 0 else 0
-                # ? STRING PART
-                # abs() absolute is used to prevent negative values
-                ba_hr = str(abs(b_hr)) + " hours" if abs(b_hr) > 1 else str(abs(b_hr)) + " hour" if b_hr == 1 else ""
-                ba_min = str(abs(b_min)) + " minutes" if abs(b_min) > 1 else str(abs(b_min)) + " minute" if b_min == 1 else ""
-                ba_sec = str(abs(b_sec)) + " seconds" if abs(b_sec) > 1 else str(abs(b_sec)) + " second" if b_sec == 1 else ""
-                if len(battpercent) > 2:
-                    del battpercent[:-2]
-                    del batthr[:-2]
-                    del battmin[:-2]
-                    del battsec[:-2]
-                return "Battery recharges 1% every " + ba_hr + comma_insert(ba_hr, ba_min) + ba_min + comma_insert(ba_min, ba_sec) + ba_sec
+                twodgts = lambda x: x > 9 and f'{x}' or f'0{x}' # so the number is formmated as a 2 digit number
+                s1 = str(twodgts(batthr[-2])) + ':' + str(twodgts(battmin[-2])) + ':' + str(twodgts(battsec[-2])) # before previous time
+                s2 = str(twodgts(batthr[-1])) + ':' + str(twodgts(battmin[-1])) + ':' + str(twodgts(battsec[-1])) # current time
+                FMT = '%H:%M:%S'
+                tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
+                if tdelta.days < 0:
+                    tdelta = timedelta(
+                        days=0,
+                        seconds=tdelta.seconds,
+                        microseconds=tdelta.microseconds
+                    )
+                hourminsec = int(tdelta.total_seconds())
+                if multilog == False:
+                    if len(battpercent) > 2:
+                        del battpercent[:-2]
+                        del batthr[:-2]
+                        del battmin[:-2]
+                        del battsec[:-2]
+                return "Battery recharges 1% every " + convert(hourminsec)
             return "Battery is plugged in, will try to calculate recharging or consumption later"
-
-
-
 
 # ? window appearnce
 window = Tk()
+var = IntVar()
 
+window.resizable(0,0)
 window.title("Avg. Battery Consumption")
 
-window.geometry('475x75') # 475x50, x100 only for testing
-window.configure(bg='black')
+window.geometry('475x100') # 475x50, x100 only for testing
+window.configure(bg='black', highlightbackground='black', highlightthickness=0)
 window.attributes('-topmost',True) # window will always be on top for benchmark purposes
 
-lbl = Label(window, fg='#01ff48', justify='center') # placeholder: text="Battery consumes 1% every XXX hours, XXX minutes, XXX seconds"
-lbl.grid(column=0, row=0)
+def coutput(radio):
+    try:
+        file = asksaveasfile(defaultextension=".txt", initialfile=f"{currtoday}.txt")
+        filestore.append(file.name)
+        logtime = f"{batthr[-1]}:{battmin[-1]}:{battsec[-1]} - {battpercent[-1]}%\n{batthr[-2]}:{battmin[-2]}:{battsec[-2]} - {battpercent[-2]}%\n"
+        file.write(logtime)
+        writeappend(filestore[0])
+        selection = "Logging to " + file.name
+        ti.config(text = selection)
+        radio.pack_forget()
+    except:
+        return
+
+def writeappend(filepath):
+    battery = psutil.sensors_battery()
+    if filepath != "":
+        if (battery.percent < battpercent[-1]) or (battery.percent > battpercent[-1]):
+            with open(filepath, "a") as f:
+                twodgts = lambda x: x > 9 and f'{x}' or f'0{x}'
+                dt = datetime.now()
+                battstat = batteryConsumption(dt, batthr, battmin, battsec, battpercent)
+                stuff = f"\n{twodgts(batthr[-1])}:{twodgts(battmin[-1])}:{twodgts(battsec[-1])} {battpercent[-1]}% - {battstat}"
+                f.write(stuff)
+                window.after(2000, writeappend, filepath)
+
+def openlog():
+    try:
+        filename = filestore[0]
+        if platform.system() == 'Darwin':       # macOS
+            subprocess.call(('open', filename))
+        elif platform.system() == 'Windows':    # Windows
+            os.startfile(filename)
+        else:                                   # linux variants
+            subprocess.call(('xdg-open', filename))
+    except Exception:
+        ti.configure(text = "No log file found, try save continuous output first")
+
+lbl = Label(window, fg='#01ff48', bg='black', justify='center') # placeholder: text="Battery consumes 1% every XXX hours, XXX minutes, XXX seconds"
 lbl.place(relx=.5, rely=.2, anchor="center") # default very center: relx=.5, rely=.5, anchor="center"
 
-# time = Label(window, fg='#01ff48', justify='center')
-# time.grid(column=0, row=1)
-
-timelog =  Label(window, fg='#808080', justify='center')
-timelog.grid(column=0, row=1)
+timelog =  Label(window, fg='#808080', bg='black', justify='center')
 timelog.place(relx=.5, rely=.60, anchor="center")
 
+ti = Label(window, fg='#808080', bg='black', justify='center', font=(None, 8), bd=1, relief=SUNKEN, anchor=W)
+ti.pack(side=BOTTOM, fill=X) # relx=.5, rely=.75, anchor="center"
 
+R1 = Radiobutton(window, text="Save continuous output", variable=var, value=1,
+                    font=(None, 6), command=lambda: coutput(R1), bg='black')
+R1.pack( anchor = SE, side = 'bottom' )
+
+B = Button(window, text="Open Log", command= openlog, bd=0, relief=SUNKEN, bg='black', font=(None, 6))
+B.pack(side=BOTTOM, anchor=SE)
     
-def my_mainloop():
-    dt = datetime.datetime.now()
+def mmainloop():
+    dt = datetime.now()
     battstat = batteryConsumption(dt, batthr, battmin, battsec, battpercent)
     lbl.configure(text=battstat)
     if len(battpercent) > 1:
         logtime = f"{batthr[-1]}:{battmin[-1]}:{battsec[-1]} - {battpercent[-1]}%\n{batthr[-2]}:{battmin[-2]}:{battsec[-2]} - {battpercent[-2]}%"
         timelog.configure(text=logtime)
-    
-    # ? for testing only
-    #dts = dt.strftime("%d-%m-%Y %H:%M:%S %p")
-    #batty = psutil.sensors_battery()
-    #timelog.configure(text=f"{dts} {batty.percent}% {batthr} {battmin} {battsec} {battpercent}")
-    window.after(1000, my_mainloop)    
+    window.after(1000, mmainloop)    
 
-window.after(1000, my_mainloop)
+window.after(1000, mmainloop)
 
 
 window.mainloop()
